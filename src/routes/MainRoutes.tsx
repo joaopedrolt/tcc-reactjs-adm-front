a9133392963a46eb52d4e9550950d570112660bf
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2022-11-09 00:42:59 -0300

    -----

---
+++ src/routes/MainRoutes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+function MainRoutes() {
+
+
+    return (
+      <>
+        <Route ></Route>
+        <Routes>
+
+
+        </Routes> 
+      </>
+    )
+  }
+  
+  export default MainRoutes;
+  
